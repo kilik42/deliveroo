68f9b7b885550ef3a25ebcbc46feb513532c3a19
kilik42 <marvinevins@yahoo.com>
2022-07-10 14:06:41 -0500

    working on fixing issues

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -9,7 +9,7 @@ const CategoryCard = ({imgUrl, title}) => {
         }}
         className="h-20 w-20 rounded"
         />
-      <Text className="absolute bottom-1 left-1">{title}</Text>
+      <Text className="absolute bottom-1 left-1 text-white font-bold">{title}</Text>
     </TouchableOpacity>
   )
 }
