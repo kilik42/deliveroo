3b53d57d2074b772b069bcaf491feebe1e39cff7
marvin evins <marvinevins@yahoo.com>
2022-08-25 10:53:37 -0500

    updating categories page

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -2,15 +2,18 @@
 import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import CategoryCard from './CategoryCard'
-import sanityClient from "../sanity";
+import sanityClient, { urlFor } from "../sanity";
 const Categories = () => {
 
   const [categories, setCategories] = useState([]);
 
   useEffect(()=>{
     sanityClient.fetch(`
-      
+      *[_type == "category"]
     `)
+    .then((data)=>{
+      setCategories(data);
+    })
   },[])
   return (
     <ScrollView 
@@ -23,12 +26,20 @@ const Categories = () => {
     showsHorizontalScrollIndicator={false}
     >
         {/* Category card */}
-        <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 1"/>
+
+        {categories.map((category)=>(
+          <CategoryCard
+          key={category._id}
+          imgUrl = {urlFor(category.image).url()}
+          title= {category.name}
+          />
+        ))}
+        {/* <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing 1"/>
         <CategoryCard imgUrl="https://links.papareact.com/wru" title="Testing 2"/>
         <CategoryCard imgUrl="https://links.papareact.com/wru" title="Testing 3"/>
         <CategoryCard imgUrl="https://links.papareact.com/wru" title="Testing 3"/>
         <CategoryCard imgUrl="https://links.papareact.com/wru" title="Testing 3"/>
-        <CategoryCard imgUrl="https://links.papareact.com/wru" title="Testing 3"/>
+        <CategoryCard imgUrl="https://links.papareact.com/wru" title="Testing 3"/> */}
       <Text>Categories</Text>
     </ScrollView>
   )
